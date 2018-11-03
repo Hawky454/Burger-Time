5a94178e598fc55e70adeca99ba9b8923e6a634d
David Miller <hawky454@gmail.com>
2018-11-02 23:53:52 -0500

    just finished lecture 123

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const Burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingredientKey => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
+        return (
+          <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
+        );
+      });
+    })
+    .reduce((array, element) => {
+      return array.concat(element);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <h1>Please start adding ingredients!</h1>;
+  }
+  console.log(transformedIngredients);
+  return (
+    <div className={styles.burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default Burger;
