cc4affe93d73e973d43786efa01498fccf61c71f
David Miller <hawky454@gmail.com>
2018-11-06 21:03:56 -0600

    finished working on OrderSummary

---
+++ src/components/Burger/OrderSummery/OrderSummery.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Aux from '../../../hoc/Aux';
+
+const OrderSummary = props => {
+  const ingredientSummary = Object.keys(props.ingredients).map(
+    ingredientKey => {
+      // the below, hardcoded would look like: "<li>Salad: 1</li>"
+      return (
+        <li>
+          <span style={{ textTransform: 'capitalize' }}>{ingredientKey}</span>:{' '}
+          {props.ingredients[ingredientKey]}
+        </li>
+      );
+    }
+  );
+
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>Continue to checkout?</p>
+    </Aux>
+  );
+};
+
+export default OrderSummary;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Aux from '../../hoc/Aux';
 import styles from './Layout.module.css';
 
-const Layout = (props) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={styles.content}>
-            {props.children}
-        </main>
-    </Aux>
+const Layout = props => (
+  <Aux>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={styles.content}>{props.children}</main>
+  </Aux>
 );
 
 export default Layout;
