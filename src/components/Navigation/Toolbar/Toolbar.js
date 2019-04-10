eaaa2db94c1b0536484a7127835e64cb698bcae0
David Miller <hawky454@gmail.com>
2019-04-10 18:38:00 -0500

    made changes to the side drawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Toolbar.module.css';
 import Logo from '../../Logo/Logo';
 import NavItems from '../NavItems/NavItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const Toolbar = props => (
   <header className={styles.toolbar}>
