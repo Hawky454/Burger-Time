fb73ea6c2481ff25bf7812aa758f529867361ed2
David Miller <hawky454@gmail.com>
2020-04-07 07:51:33 +0000

    new comment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-// This is so fucking cool!  I am able to update and play with this as if I'm working on my home computer! 4/7/20
+// This is so fucking cool!  I am able to update and play with this as if I'm working on my home computer! 4/7/20 **REMEMBER that this 
+//is saving github to a new branch call Hawky454/gitpod-setup!
 
 class App extends Component {
   render() {
