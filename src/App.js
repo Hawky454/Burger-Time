06328e6f851a7d8b4a0a7c71f148c4195a37d2e5
David Miller <hawky454@gmail.com>
2020-04-07 07:41:24 +0000

    trying out gitpod from work and I have to say I am impressed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
+// This is so fucking cool!  I am able to update and play with this as if I'm working on my home computer! 4/7/20
+
 class App extends Component {
   render() {
     return (
